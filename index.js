82b188bb83273fb2bdfde4400945d76372f4d081
Shivani Das <dasshivani313@gmail.com>
2026-08-20 17:47:59 +0530

    fixed

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-// const jsonServer = require("json-server"); // importing json-server library
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
-
-const jsonServer = require("json-server");
-
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(port);
